38b8536bf675f47a3ddb591cb783716898069772
avickgit <avick@ufl.edu>
2024-12-04 12:09:44 -0500

    Modified main function's display menu appearance, added while-loop to facilitate user input.

--- main.cpp
+++ main.cpp
@@ -10,74 +10,96 @@
 
 using namespace std;
 
+void displayMenu() {
+    cout << "=================================" << endl;
+    cout << "Please select an option:" << endl;
+    cout << "1. Quick Sort" << endl;
+    cout << "2. Heap Sort" << endl;
+    cout << "3. Exit" << endl;
+    cout << "=================================" << endl;
+
+}
 
 int main() {
-    int choice;
-    cout
-            << "Welcome to Signals and Systems. This program will take a csv file and sort the data to identify the ratio of legitimate and phishing websites."
-            << endl;
-    cout
-            << "We will be comparing the Heap sort and Quick Sort algorithms to compare their performance while completing the same task \n Enter 1 for Quick Sort and 2 for Heap Sort"
-            << endl;
-    cin >> choice;
 
-    if (choice == 1) {
-        string inputFilename = "project3b_data.csv";         // Input file name
-        //string outputFilename = "quick_sorted_data_urls.csv";     // Output file name
-        DataProcessor dataObj;
-        QuickSort sorter;
+    cout << "Welcome to Signals of Suspicion." << endl;
+    cout << "This program will take a CSV file and sort the data to prioritize website URLs based on their legitimacy." << endl;
+    cout << "We will compare the algorithm performance of Quick Sort and Heap Sort while completing the same task." << endl;
 
-        vector<DataProcessor::DatasetRow> dataset = dataObj.readDataset(inputFilename);
+    int choice = -1;
 
-        vector<DataProcessor::DatasetRow> legitimate, suspicious;
-        for (DataProcessor::DatasetRow pair : dataset){
-            if (pair.status == 0){
-                suspicious.push_back(pair);
+    do {
+        displayMenu();
+        cin >> choice;
+
+        if (choice == 1) {
+            string inputFilename = "project3b_data.csv";         // Input file name
+            DataProcessor dataObj;
+            QuickSort sorter;
+
+            vector<DataProcessor::DatasetRow> dataset = dataObj.readDataset(inputFilename);
+
+            vector<DataProcessor::DatasetRow> legitimate, suspicious;
+            for (DataProcessor::DatasetRow pair : dataset){
+                if (pair.status == 0){
+                    suspicious.push_back(pair);
+                }
+                else if (pair.status == 1){
+                    legitimate.push_back(pair);
+                }
             }
-            else if (pair.status == 1){
-                legitimate.push_back(pair);
-            }
+
+            cout <<"Sorting dataset using Quick Sort..." << endl;
+            sorter.quickSorting(suspicious);
+            sorter.quickSorting(legitimate);
+
+            cout << "Writing sorted dataset to files..." << endl;
+            dataObj.writeDataset("quicksort_suspicious_urls.csv", suspicious);
+            dataObj.writeDataset("quicksort_legitimate_urls.csv", legitimate);
+
+            cout << "Sorting completed successfully!" << endl;
         }
 
-        cout <<"Sorting dataset using Quick Sort..." << endl;
-        sorter.quickSorting(suspicious);
-        sorter.quickSorting(legitimate);
+        else if (choice == 2) {
+            string inputFilename = "project3b_data.csv";         // Input file name
+            DataProcessor dataObj;
+            HeapSort sorter;
 
-        cout << "Writing sorted dataset to files..." << endl;
-        dataObj.writeDataset("quicksort_suspicious_urls.csv", suspicious);
-        dataObj.writeDataset("quicksort_legitimate_urls.csv", legitimate);
+            vector<DataProcessor::DatasetRow> dataset = dataObj.readDataset(inputFilename);
 
-        cout << "Sorting completed successfully!" << endl;
-    }
-
-    else if (choice == 2) {
-        string inputFilename = "project3b_data.csv";         // Input file name
-        //string outputFilename = "heap_sorted_data_urls.csv";     // Output file name
-        DataProcessor dataObj;
-        HeapSort sorter;
-
-        vector<DataProcessor::DatasetRow> dataset = dataObj.readDataset(inputFilename);
-
-        vector<DataProcessor::DatasetRow> legitimate, suspicious;
-        for (DataProcessor::DatasetRow pair : dataset){
-            if (pair.status == 0){
-                suspicious.push_back(pair);
+            vector<DataProcessor::DatasetRow> legitimate, suspicious;
+            for (DataProcessor::DatasetRow pair : dataset){
+                if (pair.status == 0){
+                    suspicious.push_back(pair);
+                }
+                else if (pair.status == 1){
+                    legitimate.push_back(pair);
+                }
             }
-            else if (pair.status == 1){
-                legitimate.push_back(pair);
-            }
+            cout <<"Sorting dataset using Heap Sort..." << endl;
+            sorter.heapSort(suspicious);
+            sorter.heapSort(legitimate);
+
+            cout << "Writing sorted dataset to files..." << endl;
+            dataObj.writeDataset("heapsort_suspicious_urls.csv", suspicious);
+            dataObj.writeDataset("heapsort_legitimate_urls.csv", legitimate);
+
+            cout << "Sorting completed successfully!" << endl;
+
         }
-        cout <<"Sorting dataset using Heap Sort..." << endl;
-        sorter.heapSort(suspicious);
-        sorter.heapSort(legitimate);
 
-        cout << "Writing sorted dataset to files..." << endl;
-        dataObj.writeDataset("heapsort_suspicious_urls.csv", suspicious);
-        dataObj.writeDataset("heapsort_legitimate_urls.csv", legitimate);
+        else if (choice == 3) {
+            break;
+        }
 
-        cout << "Sorting completed successfully!" << endl;
+        else {
+            cout << "That is an invalid input." << endl;
+        }
 
-    } else {
-        cout << "That was an invalid input. Please restart the program and enter 1 or 2." << endl;
-    }
+    } while (choice != 3);
+
+    cout << "Thank you for using Signals of Suspicion!" << endl;
+    cout << "Have a good day!" << endl;
+
+    return 0;
 }
